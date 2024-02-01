8d7f0599f8afe48377f398a4edaa6a7dc9878cac
priyesh37 <priyeshthakker3100@gmail.com>
2024-02-02 02:07:18 +0530

    delete blog api

--- src/main/java/com/driver/services/BlogService.java
+++ src/main/java/com/driver/services/BlogService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class BlogService {
@@ -49,10 +50,13 @@ public class BlogService {
 
     public void deleteBlog(int blogId){
         //delete blog and corresponding images
-
-        Blog blog = blogRepository1.findById(blogId).get();
-        if(blog != null)
+        Optional<Blog> byId = blogRepository1.findById(blogId);
+        if(!byId.isEmpty()){
             blogRepository1.deleteById(blogId);
-
+        }
+//        Blog blog = blogRepository1.findById(blogId).get();
+//        if(blog != null)
+//            blogRepository1.deleteById(blogId);
+//
     }
 }
